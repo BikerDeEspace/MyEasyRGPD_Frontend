b2caa94cb9a59542d667964eafa37711034e0b52
BikerDeEspace <lionel.legrand62@gmail.com>
2019-06-24 17:04:28 +0200

    remove sources

--- app/src/src/api/model/template.model.ts
+++
@@ -1,9 +0,0 @@
-import { BaseModel } from '@api/model/base.model'
-
-export class Template extends BaseModel {
-  public enabled: boolean;
-  public name: string;
-  public description: string;
-  public imported_file_name: string;
-
-}
