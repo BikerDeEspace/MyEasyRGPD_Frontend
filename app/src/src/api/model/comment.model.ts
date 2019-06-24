27e065b928fb987238ff2ff23b1592279c96deac
BikerDeEspace <lionel.legrand62@gmail.com>
2019-06-24 17:35:09 +0200

    remove app folder

--- app/src/src/api/model/comment.model.ts
+++
@@ -1,9 +0,0 @@
-import { BaseModel } from '@api/model/base.model'
-
-export class Comment extends BaseModel {
-  public id: any;
-  public pia_id: any;
-  public reference_to: string;
-  public description: string;
-  public for_measure: boolean;
-}
--- app/src/src/utils/core.ts
+++
@@ -1,2 +0,0 @@
-export { StringUtil } from '@utils/string.util';
-export { ArrayUtil } from '@utils/array.util';
